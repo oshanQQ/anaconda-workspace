a2cc3973f5fe0942ad302221c7e439af37b194f2
oshanQQ <oshanqq@gmail.com>
2022-06-07 14:19:35 +0900

    🖼️ Get images

---
+++ gan-love/get_images.sh
@@ -0,0 +1,8 @@
+googleimagesdownload --keywords "永野芽郁" --size large --limit 100 --chromedriver chromedriver 
+googleimagesdownload --keywords "浜辺美波" --size large --limit 100 --chromedriver chromedriver 
+googleimagesdownload --keywords "新垣結衣" --size large --limit 100 --chromedriver chromedriver 
+googleimagesdownload --keywords "広瀬すず" --size large --limit 100 --chromedriver chromedriver 
+googleimagesdownload --keywords "橋本環奈" --size large --limit 100 --chromedriver chromedriver 
+googleimagesdownload --keywords "白石麻衣" --size large --limit 100 --chromedriver chromedriver 
+googleimagesdownload --keywords "今田美桜" --size large --limit 100 --chromedriver chromedriver 
+googleimagesdownload --keywords "吉岡里帆" --size large --limit 100 --chromedriver chromedriver 
